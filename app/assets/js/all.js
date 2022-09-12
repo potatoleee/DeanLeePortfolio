6bd532be9416d1e8bceb2103a2ff434cabf842a2
dean <dean19990226@gmail.com>
2022-09-12 15:45:40 +0800

    check

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -16,7 +16,7 @@ AOS.init({
     
   
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 120, // offset (in px) from the original trigger point
+    offset: 110, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 1000, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
