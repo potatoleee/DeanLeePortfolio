66054b96b0d53a690a663186c83e407278782726
李東穎 <dean19990226@gmail.com>
2022-09-14 14:57:44 +0800

    picture check

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -49,7 +49,7 @@ $(".liveToast").toast({
 
 // scoll top
     $('.pagetop').on('click',function(){
-        $('html,body').animate({scrollTop:0 } ,1000);
+        $('html,body').animate({scrollTop:0 } ,100);
  });// scoll end
 
 //  漢堡選單 start
