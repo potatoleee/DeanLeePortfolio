53abb0b1c10a69036bafba718836c575846951b3
potatolee <dean19990226@gmail.com>
2023-03-21 23:33:31 +0800

    Update 2023-03-21T15:33:29.416Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -149,27 +149,4 @@ $(document).ready(function () {
 
   Cocoen.parse(document.body);
 }); // document ready end
-
-var swiper = new Swiper(".commentSwiper", {
-  slidesPerView: 1,
-  // 倒底會回第一張
-  loop: true,
-  // 有fade效果才不會整張切換
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
-}); // datepicker
-
-var elem = document.querySelector('input[name="foo"]');
-var datepicker = new Datepicker(elem, {// ...options
-});
 //# sourceMappingURL=all.js.map
